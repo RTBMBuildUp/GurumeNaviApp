b5ee649892349c5e773cffe0ba7f0c000a093a38
RTBMBuildUp <e1n18088@oit.ac.jp>
2019-06-30 04:51:47 +0900

    add recycler view

---
+++ app/src/main/java/com/example/gurumenaviapp/recyclerview/SearchResultAdapter.java
@@ -0,0 +1,55 @@
+package com.example.gurumenaviapp.recyclerview;
+
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.gurumenaviapp.R;
+import com.example.gurumenaviapp.data.ShowedInformation;
+
+import java.util.List;
+
+public class SearchResultAdapter extends RecyclerView.Adapter<SearchResultViewHolder> {
+    private List<ShowedInformation> showedInformationList;
+
+    private View view;
+
+    public SearchResultAdapter(List<ShowedInformation> showedInformationList) {
+        this.showedInformationList = showedInformationList;
+    }
+
+    @NonNull
+    @Override
+    public SearchResultViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        view = LayoutInflater
+                .from(viewGroup.getContext())
+                .inflate(R.layout.search_result, viewGroup, false);
+
+        return new SearchResultViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchResultViewHolder searchResultViewHolder, int position) {
+        final Resources resources = view.getResources();
+        final String notFound = resources.getString(R.string.notFound);
+
+        final ShowedInformation result = showedInformationList.get(position);
+
+        searchResultViewHolder.name.setText(getOrElse(result.getName(), notFound));
+        searchResultViewHolder.address.setText(getOrElse(result.getAddress(), notFound));
+        searchResultViewHolder.tel.setText(getOrElse(result.getTel(), notFound));
+
+        searchResultViewHolder.imageView.setImageBitmap(result.getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return showedInformationList.size();
+    }
+
+    private String getOrElse(String defaultValue, String option) {
+        return defaultValue == null ? option : defaultValue;
+    }
+}
---
+++ app/src/main/java/com/example/gurumenaviapp/recyclerview/SearchResultViewHolder.java
@@ -0,0 +1,25 @@
+package com.example.gurumenaviapp.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.gurumenaviapp.R;
+
+public class SearchResultViewHolder extends RecyclerView.ViewHolder {
+    public TextView name;
+    public TextView tel;
+    public TextView address;
+
+    public ImageView imageView;
+
+    SearchResultViewHolder(View view) {
+        super(view);
+
+        name = view.findViewById(R.id.RestNameTextView);
+        tel = view.findViewById(R.id.telTextView);
+        address = view.findViewById(R.id.addressTextView);
+
+        imageView = view.findViewById(R.id.imageView);
+    }
+}
