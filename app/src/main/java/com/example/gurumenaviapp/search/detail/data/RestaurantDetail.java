24727a25471f99546b2a8a7acb678a1e525c067e
RTBMBuildUp <e1n18088@oit.ac.jp>
2019-07-04 01:27:58 +0900

    divide the RestaurantThumbnail and RestaurantDetail from ShowedInformation class

---
+++ app/src/main/java/com/example/gurumenaviapp/search/detail/data/RestaurantDetail.java
@@ -0,0 +1,60 @@
+package com.example.gurumenaviapp.search.detail.data;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import com.example.gurumenaviapp.gson.data.Access;
+import com.example.gurumenaviapp.gson.data.Rest;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class RestaurantDetail {
+    private String name;
+    private String tel;
+    private String address;
+    private Access access;
+    private String openTime;
+
+    private Bitmap image;
+
+    public RestaurantDetail() {
+    }
+
+    public RestaurantDetail(Rest restaurant) {
+        this.name = restaurant.getName();
+        this.tel = restaurant.getTel();
+        this.address = restaurant.getAddress();
+        this.access = restaurant.getAccess();
+        this.openTime = restaurant.getOpentime();
+
+        try {
+            this.image = BitmapFactory.decodeStream(new URL(restaurant.getImageUrl().getShopImage()).openStream());
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Access getAccess() {
+        return access;
+    }
+
+    public String getOpenTime() {
+        return openTime;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+}
